299638f9b1ec9e57f9f7d647051b8c4c0aec42d3
Mohammed Mustafa Fulwala <33564079+mospy26@users.noreply.github.com>
2022-09-29 20:45:59 +1000

    Time: 31 ms (51.32%), Space: 11.3 MB (18.12%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,12 +1,24 @@
+#include <algorithm>
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i+1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target)
-                    return vector<int> {i, j};
-            }
+    vector<int> twoSum (std::vector<int>& nums, int target) {
+        std::map<int, int> m;
+        
+        // for each element in list, compute diff with target and check if we already
+        // visited an element in the nums list whose value is diff
+        // We can store all visited elements in a map so that access time is (roughly) O(1).
+        for (int i = 0; i < nums.size (); i++)
+        {
+            int diff = target - nums[i];
+            if (m.find (diff) != m.end ())
+                return {i, m[diff]};
+
+            m[nums[i]] = i;
         }
-        return vector<int>();
+        
+        // should never reach here as there is always a solution
+        // as promised by the question
+        return {0, 0};
     }
 };
