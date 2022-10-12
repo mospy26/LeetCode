59e48f7ea8d6c6958accc5daffce6a27a41555c8
Mohammed Mustafa Fulwala <33564079+mospy26@users.noreply.github.com>
2022-10-13 00:29:59 +1100

    Time: 8 ms (15.55%), Space: 7 MB (6.50%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -3,7 +3,7 @@ public:
     bool isHappy(int n) 
     {
         int result;
-        for (int i = 0; i <= 50; i++)
+        for (int i = 0; i <= 20; i++)
         {
             result = sumOfSquares (n);
             if (result == 1)
