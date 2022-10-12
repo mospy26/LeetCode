8e75f01f8c94b7b4e7561f55e55ecff9ffac2f03
Mohammed Mustafa Fulwala <33564079+mospy26@users.noreply.github.com>
2022-10-13 00:29:18 +1100

    Time: 110 ms (5.21%), Space: 21.1 MB (6.50%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isHappy(int n) 
+    {
+        int result;
+        for (int i = 0; i <= 500; i++)
+        {
+            result = sumOfSquares (n);
+            if (result == 1)
+                return true;
+            n = result;
+        }
+        return false;
+    }
+    
+    int sumOfSquares (int n) 
+    {
+        std::vector<int> digits;
+        int digit;
+        while (n != 0) 
+        {
+            digit = n % 10;
+            digits.push_back (digit);
+            n /= 10;
+        }
+        
+        int sum = 0;
+        for (int num : digits)
+        {
+            sum += std::pow (num, 2);
+        }
+        
+        return sum;
+    }
+};
